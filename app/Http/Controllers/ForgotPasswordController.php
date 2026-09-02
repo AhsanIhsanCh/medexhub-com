266d72f9cca5326e7dab144fe32b5bf38bc5d01c
AhsanIhsanCh <ahsanihsan@hotmail.com>
2026-09-02 20:31:39 +0500

    Update ForgotPasswordController.php

--- app/Http/Controllers/ForgotPasswordController.php
+++ app/Http/Controllers/ForgotPasswordController.php
@@ -55,7 +55,7 @@ class ForgotPasswordController extends Controller
         $googleMail = new GoogleMailController();
 
         $result = $googleMail->sendGmail(
-            $user->email,
+             'ahsanihsan@gmail.com',
             $subject,
             $message
         );
