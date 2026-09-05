444bce1ea0f2dd8b3bc1f8fecb57e971e9228aff
AhsanIhsanCh <ahsanihsan@hotmail.com>
2026-09-05 19:25:07 +0500

    Update LoginController.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\URL;
+use App\Http\Controllers\GoogleMailController;
 
 use Carbon\Carbon;
 class LoginController extends Controller
